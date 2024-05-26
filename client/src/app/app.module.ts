8cf4e0c0a9f39ed734aea6c433250e5044bc3698
theSoloBrewer <computerzombie@msn.com>
2024-05-26 11:48:23 +0200

    Update and clean ng and node

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,21 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule }	from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi }	from '@angular/common/http';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule], providers: [
+        provideClientHydration(),
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
 
